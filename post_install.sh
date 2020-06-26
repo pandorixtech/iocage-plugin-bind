96ba02e3033229034333f6fc0b498e39f789e044
Pandorix Tech <tech@pandorix.net>
2020-06-26 18:21:54 +0100

    working on initial settings

--- post_install.sh
+++ post_install.sh
@@ -3,5 +3,9 @@
 # Enable the service
 sysrc -f /etc/rc.conf named_enable="YES"
 
+# Configuration
+touch /usr/local/etc/namedb/zones.register
+echo "include \"/usr/local/etc/namedb/zones.register\";" >> /usr/local/etc/namedb/named.conf
+sed -i .orig "s/^	listen-on	{ 127.0.0.1; };//" /usr/local/etc/nameddb/named.conf
 # Start the service
 service named start 2>/dev/null
