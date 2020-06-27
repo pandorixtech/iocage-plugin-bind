e617f55ed4f657f7f13c04313f5499fb5373fc66
Pandorix Tech <tech@pandorix.net>
2020-06-27 10:03:14 +0100

    adding zone sample

--- post_install.sh
+++ post_install.sh
@@ -7,5 +7,20 @@ sysrc -f /etc/rc.conf named_enable="YES"
 touch /usr/local/etc/namedb/zones.register
 echo "include \"/usr/local/etc/namedb/zones.register\";" >> /usr/local/etc/namedb/named.conf
 sed -i .orig "s/^	listen-on	{ 127.0.0.1; };//" /usr/local/etc/namedb/named.conf
+
+echo "// ZONES list" >> /usr/local/etc/namedb/zones.register
+echo "// **********" >> /usr/local/etc/namedb/zones.register
+echo "// " >> /usr/local/etc/namedb/zones.register
+echo "// sample:" >> /usr/local/etc/namedb/zones.register
+echo "// zone \"example.local\" \t{ type master; file \"/usr/local/namedb/dynamic/example.local.db\"; };" >> /usr/local/etc/namedb/zones.register
+
+touch /usr/local/namedb/dynamic/example.local.db
+echo "\$TTL 3H" >>  /usr/local/namedb/dynamic/example.local.db
+echo "@ SOA @ nobody.localhost. 42 	1d 	 12h 	1w 	3h" >>  /usr/local/namedb/dynamic/example.local.db
+echo ";                         Serial, Refresh, Retry, Expire, Neg. cache TTL" >>  /usr/local/namedb/dynamic/example.local.db
+echo " " >>  /usr/local/namedb/dynamic/example.local.db
+echo "@	NS 	@" >>  /usr/local/namedb/dynamic/example.local.db
+echo "@	A 	127.0.0.1" >>  /usr/local/namedb/dynamic/example.local.db
+
 # Start the service
-service named start 2>/dev/null
+service named start 2 > /dev/null
