1ebaa120e213524ed269f7139e5ba1ae9beca43e
Pandorix Tech <tech@pandorix.net>
2020-06-27 14:19:30 +0100

    fixing typo

--- post_install.sh
+++ post_install.sh
@@ -9,7 +9,7 @@ echo "include \"/usr/local/etc/namedb/zones.register\";" >> /usr/local/etc/named
 sed -i .orig "s/^	listen-on	{ 127.0.0.1; };//" /usr/local/etc/namedb/named.conf
 
 chown bind:bind /usr/local/etc/namedb/zones.register
-chown bind:bind /usr/local/etc/namedb/dynamic.local.db
+chown bind:bind /usr/local/etc/namedb/dynamic/example.local.db
 
 
 # Start the service
